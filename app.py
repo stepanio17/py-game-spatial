9f8ff62a4da4af82049cccb8cac1439a01c1941a
stepanio17 <stepanbesttheeldest@mail.ru>
2026-06-25 14:40:02 +0700

    Update to load the game locally

--- app.py
+++ app.py
@@ -885,5 +885,4 @@ def back_to_menu(n_clicks, state):
     return no_update, no_update, no_update
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8050))
-    app.run(debug=False, host='0.0.0.0.', port=port)
+    app.run(debug=True, host='127.0.0.1', port=8050)
